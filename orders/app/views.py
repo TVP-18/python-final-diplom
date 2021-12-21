e4b345785d971dd9986902c3418f81a05bb2baea
Popova Tatiana <tatiana_mail@bk.ru>
2021-12-21 11:38:52 +0700

    Подключен TokenAuthentication

--- orders/app/views.py
+++ orders/app/views.py
@@ -19,27 +19,32 @@ class PartnerPriceLoad(APIView):
     Класс для загрузки/обновления прайса от поставщика
     """
     def post(self, request, *args, **kwargs):
+        print(request, request.data, request.user)
         # проверить аутентификацию пользователя
+        if not request.user.is_authenticated:
+            return JsonResponse({'Status': False, 'Error': 'Пользователь не зарегистрирован'}, status=403)
 
         # проверить права пользователя, доступно только для магазинов
+        if request.user.type != 'shop':
+            return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
 
-        # получаем файл
-        url = request.data.get('url')
-        # print(request, request.data, url)
-        if url:
-            validate_url = URLValidator()
-            try:
-                validate_url(url)
-            except ValidationError as e:
-                return JsonResponse({'Status': False, 'Error': str(e)})
-            else:
-
-                stream = get(url).content
-                print(stream)
-                data = load(stream, Loader=Loader)
-                print(data)
-
-                return JsonResponse({'Status': True})
+        # # получаем файл
+        # url = request.data.get('url')
+        # # print(request, request.data, url)
+        # if url:
+        #     validate_url = URLValidator()
+        #     try:
+        #         validate_url(url)
+        #     except ValidationError as e:
+        #         return JsonResponse({'Status': False, 'Error': str(e)})
+        #     else:
+        #
+        #         stream = get(url).content
+        #         print(stream)
+        #         data = load(stream, Loader=Loader)
+        #         print(data)
+        #
+        #         return JsonResponse({'Status': True})
 
         return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
 
