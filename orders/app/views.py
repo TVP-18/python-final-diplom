98175376d6cc411814ae51ee79c0e6ae0aaa28c3
Tatiana Popova <tatiana_mail@bk.ru>
2021-12-25 12:03:25 +0700

    Добавлен класс PartnerPriceLoad для загрузки/обновления прайса от поставщика (partner/load)

--- orders/app/views.py
+++ orders/app/views.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import authenticate
 from requests import get
 
+from django.contrib.auth.password_validation import validate_password
+
 from django.core.validators import URLValidator
 from django.core.exceptions import ValidationError
 
@@ -8,13 +10,31 @@ from django.http import JsonResponse
 
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
+# from rest_framework.viewsets import ModelViewSet
+
 
 from rest_framework.authtoken.models import Token
 
 import yaml
 
-from app.models import Category, Shop
+from app.models import Category, Shop, ProductInfo, Product, Parameter, ProductParameter
 from app.serializer import CategorySerializer, ShopSerializer
+
+
+# class CategoryViewSet(ModelViewSet):
+#     """
+#     Просмотр категорий
+#     """
+#     queryset = Category.objects.all()
+#     serializer_class = CategorySerializer
+#
+#
+# class ShopViewSet(ModelViewSet):
+#     """
+#     Просмотр магазинов
+#     """
+#     queryset = Shop.objects.filter(state=True)
+#     serializer_class = ShopSerializer
 
 
 class PartnerPriceLoad(APIView):
@@ -32,25 +52,38 @@ class PartnerPriceLoad(APIView):
             return JsonResponse({'Status': False, 'Error': 'Только для магазинов'}, status=403)
 
         # получаем файл
-        url = request.data.get('url')
+        file = request.data.get('url')
+        # print(request.data, file)
 
-        print(1, request.data)
-        print(2, url)
+        if file:
+            data = yaml.load(file, Loader=yaml.FullLoader)
+            # print(request.data, file, data)
 
+            shop, _ = Shop.objects.get_or_create(name=data['shop'], user_id=request.user.id)
 
-        if url:
-            validate_url = URLValidator()
-            try:
-                validate_url(url)
-            except ValidationError as e:
-                return JsonResponse({'Status': False, 'Error': str(e)})
-            else:
-                stream = get(url).content
-                print(stream)
-                data = yaml.load(stream, Loader=yaml.Loader)
-                print(data)
-        #
-                return JsonResponse({'Status': True})
+            for category in data['categories']:
+                category_object, _ = Category.objects.get_or_create(id=category['id'], name=category['name'])
+                category_object.shops.add(shop.id)
+                category_object.save()
+
+            ProductInfo.objects.filter(shop_id=shop.id).delete()
+            for item in data['goods']:
+                product, _ = Product.objects.get_or_create(name=item['name'], category_id=item['category'])
+
+                product_info = ProductInfo.objects.create(product_id=product.id,
+                                                          external_id=item['id'],
+                                                          model=item['model'],
+                                                          price=item['price'],
+                                                          price_rrc=item['price_rrc'],
+                                                          quantity=item['quantity'],
+                                                          shop_id=shop.id)
+                for name, value in item['parameters'].items():
+                    parameter_object, _ = Parameter.objects.get_or_create(name=name)
+                    ProductParameter.objects.create(product_info_id=product_info.id,
+                                                    parameter_id=parameter_object.id,
+                                                    value=value)
+
+            return JsonResponse({'Status': True})
 
         return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
 
@@ -65,7 +98,7 @@ class CategoryView(ListAPIView):
 
 class ShopView(ListAPIView):
     """
-    Просмотр магазинов
+    Просмотр магазинов, только принимающих заказы
     """
     queryset = Shop.objects.filter(state=True)
     serializer_class = ShopSerializer
@@ -73,7 +106,7 @@ class ShopView(ListAPIView):
 
 class UserLogin(APIView):
     """
-    Авторизация пользователя по логину и паролю, возвращаем токен
+    Вход пользователя по логину и паролю, возвращаем токен
     """
 
     def post(self, request, *args, **kwargs):
@@ -90,3 +123,58 @@ class UserLogin(APIView):
             return JsonResponse({'Status': False, 'Errors': 'Не удалось авторизовать'})
 
         return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
+
+
+# class UserRegister(APIView):
+#     """
+#     Регистрация покупателя
+#     """
+#
+#     def post(self, request, *args, **kwargs):
+#
+#         # проверяем обязательные аргументы
+#         if {'first_name', 'last_name', 'email', 'password', 'company', 'position'}.issubset(request.data):
+#             errors = {}
+#
+#             # проверяем пароль
+#
+#             try:
+#                 validate_password(request.data['password'])
+#             except Exception as password_error:
+#                 return JsonResponse({'Status': False, 'Errors': {'password': 'должно быть описание ошибок'}})
+#             else:
+#         #     user_serializer = UserSerializer(data=request.data)
+#         #     if user_serializer.is_valid():
+#         #         # сохраняем пользователя
+#         #         user = user_serializer.save()
+#         #         user.set_password(request.data['password'])
+#         #         user.save()
+#         #         new_user_registered.send(sender=self.__class__, user_id=user.id)
+#         #         return JsonResponse({'Status': True})
+#         #     else:
+#         #         return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+#
+#             # try:
+#             #     validate_password(request.data['password'])
+#             # except Exception as password_error:
+#             #     error_array = []
+#             #     # noinspection PyTypeChecker
+#             #     for item in password_error:
+#             #         error_array.append(item)
+#             #     return JsonResponse({'Status': False, 'Errors': {'password': error_array}})
+#             # else:
+#             #     # проверяем данные для уникальности имени пользователя
+#             #     request.data._mutable = True
+#             #     request.data.update({})
+#             #     user_serializer = UserSerializer(data=request.data)
+#             #     if user_serializer.is_valid():
+#             #         # сохраняем пользователя
+#             #         user = user_serializer.save()
+#             #         user.set_password(request.data['password'])
+#             #         user.save()
+#             #         new_user_registered.send(sender=self.__class__, user_id=user.id)
+#             #         return JsonResponse({'Status': True})
+#             #     else:
+#             #         return JsonResponse({'Status': False, 'Errors': user_serializer.errors})
+#
+#         return JsonResponse({'Status': False, 'Errors': 'Не указаны все необходимые аргументы'})
